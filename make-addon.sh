de3beb3329eee369cbf4dcdd00cbaccc5d2c15b8
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-02-02 07:53:25 -0800

    chroot added

---
+++ make-addon.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+help() {
+  echo "
+
+   make-addon.sh OUTPUT-FILE COMMAND [ARG]...
+
+   - OUTPUT-FILE
+     Is the file to which the addon should be saved.
+     It must have either ending 'ext2' or 'squashfs'.
+   - COMMAND
+     is a command to run. It will be executed as root.
+     It can have optional arguments ARG
+  "
+}
+
+log() {
+  echo "$0 - $@"
+}
+
+error() {
+  log "$@"
+  exit 1
+}
+
+# input
+script="$1"
+output="$2"
+
+log "verifying parameters"
+if [ -z "$script" ] || [ -z "$output" ]
+then
+  help
+  exit 1
+fi
+
+log "Deriving parameters."
+root="/tmp/`basename \"$script\"`-`basename \"$output\"`-`date +%N`"
+data="${root}-data"
+
+log "Creating directories $root and $data"
+mkdir -p "$root"
+mkdir "$data" || error "$data exists."
+
+mount -t aufs -o "noatime,dirs=$data=rw:/=rr" aufs "$root"
+
+chroot "$root" "$script"
+
+umount "$root"
+
+
