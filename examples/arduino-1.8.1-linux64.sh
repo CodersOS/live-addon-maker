cfd843c68fb12386f0c02ec964c971214747dc25
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-02-02 13:22:24 -0800

    changed example ouput name

--- examples/arduino-1.8.1-linux64.sh
+++ examples/arduino-1.8.1-linux64.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo ./make-addon.sh *.iso \
-  examples/arduino-1.8.1.squashfs \
+  examples/arduino-1.8.1-linux64.squashfs \
   -b 'bash -c "mkdir /opt ; cd /opt ; wget https://downloads.arduino.cc/arduino-1.8.1-linux64.tar.xz"' \
   bash -c 'cd /opt/ && tar -xf arduino-1.8.1-linux64.tar.xz && cd arduino-1.8.1 && ./install.sh'
