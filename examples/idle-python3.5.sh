efe212f90e7a506fcabd962b581bb6ae067b4496
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-02-02 13:15:24 -0800

    updated idle for new requirements from README

--- examples/idle-python3.5.sh
+++ examples/idle-python3.5.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo ./make-addon.sh xubuntu-16.04.1-desktop-amd64.iso examples/idle-python3.5.squashfs -b 'apt-get update' bash -c 'apt-get -y install idle-python3.5'
+sudo ./make-addon.sh *.iso examples/idle-python3.5.squashfs -b 'apt-get update' bash -c 'apt-get -y install idle-python3.5'
