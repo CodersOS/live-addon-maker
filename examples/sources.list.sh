4a1000b9e94d2ee9dfda4b6a0fe4f8d7ec2a3345
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-02-06 02:14:39 -0800

    fix: added \

--- examples/sources.list.sh
+++ examples/sources.list.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo ./make-addon.sh *.iso examples/z-sources.list.squashfs \
-  -c "sed -i=.live-addon 's/restricted$/restricted multiverse universe/' /etc/apt/sources.list"
+  -c "sed -i=.live-addon 's/restricted$/restricted multiverse universe/' /etc/apt/sources.list" \
   -c "apt-get update"
