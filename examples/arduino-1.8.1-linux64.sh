d70c978244e935cc871b5e7d55a9b0908a601da4
Nicco Kunzmann <niccokunzmann@googlemail.com>
2017-02-05 19:15:08 -0800

    Made error message more clear

--- examples/arduino-1.8.1-linux64.sh
+++ examples/arduino-1.8.1-linux64.sh
@@ -5,10 +5,10 @@ desktop=""
 (
   cd /tmp
   wget -c https://downloads.arduino.cc/arduino-1.8.1-linux64.tar.xz || {
-    echo "Could not download."
+    echo "Could not download Arduino binaries."
     exit 1
   }
-)
+) || exit 1
 
 sudo ./make-addon.sh *.iso examples/z-arduino-1.8.1-linux64.squashfs \
                      -A /tmp/arduino-1.8.1-linux64.tar.xz /opt/ \
