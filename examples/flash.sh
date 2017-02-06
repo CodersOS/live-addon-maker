8aa0ef4164448cb47d65ec6247e0425a6b7b2d4f
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-02-06 02:14:05 -0800

    added sources to install flash

--- examples/flash.sh
+++ examples/flash.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo ./make-addon.sh *.iso examples/z-flash.squashfs \
+                     -C "sed -i 's/restricted$/restricted multiverse universe/' /etc/apt/sources.list" \
                      -C 'apt-get update' \
                      -c 'apt-get -y install flashplugin-installer'
