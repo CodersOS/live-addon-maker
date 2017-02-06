2c95797071cb2fb3723daf784796a03fc2e3973a
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-02-06 04:42:15 -0800

    using unsquashfs instead of mount for loop device shortage

--- merge-addons.sh
+++ merge-addons.sh
@@ -25,54 +25,12 @@ if [ -z "$output" ] || [ -z "$1" ]; then
 fi
 
 base="/tmp/`basename \"output\"`-`date '+%N'`"
-sources="$base/sources/"
-mount_order=""
+target="$base/addon"
+temp_output="$base/output"
 
-mkdir -p "$sources"
+mkdir -p "$temp_output"
 
-log "Mounting addons."
-
-umount_addons() {
-  (
-    cd "$sources"
-    for d in *; do
-      umount "$d"
-    done
-  )
-}
-
-temp_output="$base/output.squashfs"
-
-squash() {
-  mount_order="${mount_order%:}"
-
-  target="$base/output"
-  mkdir -p "$target"
-
-  mount -t aufs -o dirs="$mount_order" none "$target"
-
-  if [ -z "`which mksquashfs`" ]; then
-    apt-get -y install squashfs-tools
-  fi
-
-  mksquashfs "$target" "$temp_output" -noappend || \
-    error "Could not squash to $output"
-  umount "$target"
-  mv "$temp_output" "$output"
-
-  output_mount="$base/output-mount"
-  mkdir -p "$output_mount"
-  mount "$output" "$output_mount"
-  mount_order="$output_mount=ro"
-  squashed="true"
-  umount_addons
-}
-
-
-
-i=0
 for addon in "$@"; do
-  squashed="false"
   [ -e "$addon" ] || \
     error "Addon not found: $addon"
   # from http://superuser.com/a/196655
@@ -80,24 +38,24 @@ for addon in "$@"; do
     [ "`stat -c '%d:%i' \"$addon\"`" != "`stat -c '%d:%i' \"$output\"`" ] || {
     error "Can not use $addon as input because it is also the output."
   }
-  i=$((i + 1))
-  number="`printf '%0*d\n' 3 $i`"
-  dir="$sources/$number-`basename \"$addon\"`"
-  log "preparing addon $addon"
-  mkdir -p "$dir"
-  log "Add loopback device http://unix.stackexchange.com/a/198637/27328"
-  losetup -f || log "No more free loop devices."
-  umount "$addon"
-  # [ "$((i % 3))" != "0" ] && \
-  mount "$addon" "$dir" || \
-   { squash && mount "$addon" "$dir" ; } || \
-   error "Could not mount"
-  mount_order="$dir=ro:$mount_order"
+  [ -d "$target" ] && {
+    rm -r "$target" || \
+      error "Could not remove previous addon."
+  }
+  mkdir -p "$target"
+  unsquashfs -f -d "$target" "$addon" || \
+    error "Could not unsquash"
+  log "Attempting hard link."
+  cp -rlft "$target" "$temp_output" || \
+    cp -rft "$target" "$temp_output" || \
+    error "Could not copy addon"
 done
 
-[ "$squashed" == "true" ] || squash
+mksquashfs "$target" "$output" -noappend || \
+  error "Could not squash"
 
-umount "$output_mount"
+rm -rf "$base"
+
 exit 0
 
 
